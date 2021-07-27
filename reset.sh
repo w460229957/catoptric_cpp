3fb2707c7ed7cee42757311071e0444db33bc8a4
Scott Sirri <scottsirri@Scotts-MacBook-Air.local>
2021-07-27 13:36:00 -0500

    Cleaning up dir

--- reset.sh
+++ reset.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-mv csv/archive/0_test.csv csv/new/test.csv
+mv csv/archive/0_one.csv csv/new/one.csv
+mv csv/archive/1_one.csv csv/new/one.csv
+mv csv/archive/0_two.csv csv/new/two.csv
+mv csv/archive/1_two.csv csv/new/two.csv
 echo "Reset test csv"
